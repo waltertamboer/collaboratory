06e49e8c8e97dbbb0a26fdee46162de56e2b6396
Walter Tamboer <walter.tamboer@live.com>
2012-10-16 23:14:46 +0200

    Added the milestone entity.

---
+++ module/Application/src/Application/Entity/Milestone.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * This file is part of Collaboratory (https://github.com/nextphp/collaboratory)
+ *
+ * @link      https://github.com/nextphp/collaboratory for the canonical source repository
+ * @copyright Copyright (c) 2012 nextphp (https://github.com/nextphp)
+ * @license   For the full copyright and license information, view the LICENSE file.
+ * @package   Collaboratory
+ */
+
+namespace Application\Entity;
+
+class Milestone
+{
+    private $id;
+    private $projectId;
+    private $name;
+    private $description;
+    private $createdOn;
+    private $dueDate;
+}
