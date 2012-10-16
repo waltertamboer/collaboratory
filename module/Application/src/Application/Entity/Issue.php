e2fe7061c4e1cf4189ae183d16a35c8102333cd9
Walter Tamboer <walter.tamboer@live.com>
2012-10-16 23:32:58 +0200

    Added the issue entity

---
+++ module/Application/src/Application/Entity/Issue.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * This file is part of Collaboratory (https://github.com/nextphp/collaboratory)
+ *
+ * @link      https://github.com/nextphp/collaboratory for the canonical source repository
+ * @copyright Copyright (c) 2012 nextphp (https://github.com/nextphp)
+ * @license   For the full copyright and license information, view the LICENSE file.
+ * @package   Collaboratory
+ */
+
+namespace Application\Entity;
+
+class Issue
+{
+    private $id;
+    private $projectId;
+    private $title;
+    private $description;
+    private $createdOn;
+    private $createdBy;
+}
