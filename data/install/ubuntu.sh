20a2c212b98b5327480200d4239f53b76948fdea
Walter Tamboer <walter.tamboer@live.com>
2012-11-18 22:32:53 +0100

    Added a shell script for Git and updated the install file.

--- data/install/ubuntu.sh
+++ data/install/ubuntu.sh
@@ -7,11 +7,6 @@
 # First off, we exit when there is an error
 set -e
 
-# The parameters that are used in the script:
-MYSQL_COLLAB_DATABASE="collaboratory"
-MYSQL_COLLAB_USERNAME="collaboratory"
-MYSQL_COLLAB_PASSWORD=$(cat /dev/urandom | tr -cd [:alnum:] | head -c ${1:-16})
-
 # We assume that the target machine is a clean machine. Before we start the
 # installation we make sure the machine is up-to-date:
 sudo apt-get update
@@ -39,46 +34,14 @@ sudo adduser \
     --home /home/git \
     git
 
-# For security reasons we let Collaboratory run under its own use:
-sudo adduser --disabled-login --gecos 'collaboratory system' collaboratory
-
-# The next step is very important. Gitolite expects a SSH key of the administrator
-# and since we're using Collaboratory to manage the Git repositories, we need to
-# create one.
-sudo -H -u collaboratory ssh-keygen -q -N '' -t rsa -f /home/collaboratory/.ssh/id_rsa
-
-# The next step is to actually install Gitolite. This needs to be done under the git user:
+# Move to the new home directory:
 cd /home/git
-sudo -u git -H mkdir bin
-sudo -H -u git git clone git://github.com/sitaramc/gitolite.git /home/git/gitolite
-sudo -u git sh -c 'echo -e "PATH=\$PATH:/home/git/bin\nexport PATH" >> /home/git/.profile'
-sudo -u git sh -c 'gitolite/install -ln /home/git/bin'
 
-# Copy over the public key to the git home and then setup Gitolite:
-sudo cp /home/collaboratory/.ssh/id_rsa.pub /home/git/collaboratory.pub
-sudo chmod 0444 /home/git/collaboratory.pub
-sudo -u git -H sh -c "PATH=/home/git/bin:$PATH; gitolite setup -pk /home/git/collaboratory.pub"
-
-# We have two users now (git and collaboratory), we add them to each other's groups so
-# the system can easily manage each others directories:
-sudo usermod -a -G git collaboratory
-sudo usermod -a -G collaboratory git
-
-# Set up the permissions:
-sudo chmod -R g+rwX /home/git/repositories/
-sudo chown -R git:git /home/git/repositories/
-
-# Let's clone the admin repo of Gitolite so that our key gets recognized:
-sudo -H -u collaboratory git clone git@localhost:gitolite-admin.git /tmp/gitolite-admin
-sudo rm -rf /tmp/gitolite-admin
-
-# It's now time to install Collaboratory:
-cd /home/collaboratory
-sudo -H -u collaboratory git clone https://github.com/pixelpolishers/collaboratory.git collaboratory
+# Install Collaboratory in Git's home directory:
+sudo -H -u git git clone https://github.com/pixelpolishers/collaboratory.git /home/git
 
 # Collaboratory uses Composer to install its dependencies, let's do so:
-cd collaboratory
-sudo -H -u collaboratory php composer.phar install
+sudo -H -u git php composer.phar install
 
 # The IP address that should be used to browse to:
 IP_ADDRESS=`ifconfig | awk -F':' '/inet addr/&&!/127.0.0.1/{split($2,_," ");print _[1]}'`
@@ -90,7 +53,7 @@ echo "<virtualhost *:80>
 
     # The document index:
     DirectoryIndex index.php
-    DocumentRoot /home/collaboratory/collaboratory/public
+    DocumentRoot /home/git/public
 
     # PHP settings:
     php_value error_reporting 32767
@@ -99,8 +62,8 @@ echo "<virtualhost *:80>
 
     # Log information:
     LogLevel warn
-    ErrorLog  /home/collaboratory/collaboratory/logs/error.log
-    CustomLog /home/collaboratory/collaboratory/logs/access.log combined
+    ErrorLog  /home/git/logs/error.log
+    CustomLog /home/git/logs/access.log combined
 </virtualhost>" | sudo tee /etc/apache2/sites-available/collaboratory > /dev/null
 
 # Enable the new virtual host:
