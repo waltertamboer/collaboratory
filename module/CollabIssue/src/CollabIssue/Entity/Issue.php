437eee93515599132456ef2c9953639725225dac
Walter Tamboer <walter.tamboer@live.com>
2012-12-01 01:35:46 +0100

    Refactored the issue entity

--- module/CollabIssue/src/CollabIssue/Entity/Issue.php
+++ module/CollabIssue/src/CollabIssue/Entity/Issue.php
@@ -8,14 +8,82 @@
  * @package   Collaboratory
  */
 
-namespace Application\Entity;
+namespace CollabIssue\Entity;
+
+use CollabProject\Entity\Project;
+use DateTime;
 
 class Issue
 {
     private $id;
-    private $projectId;
+    private $project;
     private $title;
     private $description;
-    private $createdOn;
+    private $creationDate;
     private $createdBy;
+
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    public function setId($id)
+    {
+        $this->id = $id;
+        return $this;
+    }
+
+    public function getProject()
+    {
+        return $this->project;
+    }
+
+    public function setProject(Project $project)
+    {
+        $this->project = $project;
+        return $this;
+    }
+
+    public function getTitle()
+    {
+        return $this->title;
+    }
+
+    public function setTitle($title)
+    {
+        $this->title = $title;
+        return $this;
+    }
+
+    public function getDescription()
+    {
+        return $this->description;
+    }
+
+    public function setDescription($description)
+    {
+        $this->description = $description;
+        return $this;
+    }
+
+    public function getCreationDate()
+    {
+        return $this->creationDate;
+    }
+
+    public function setCreationDate($creationDate)
+    {
+        $this->creationDate = new DateTime($creationDate);
+        return $this;
+    }
+
+    public function getCreatedBy()
+    {
+        return $this->createdBy;
+    }
+
+    public function setCreatedBy($createdBy)
+    {
+        $this->createdBy = $createdBy;
+    }
 }
